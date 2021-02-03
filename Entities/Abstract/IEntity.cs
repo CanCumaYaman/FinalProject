43373a494dffe85376a224808db49ed0cc1e55a5
CanCumaYaman <65663050+CanCumaYaman@users.noreply.github.com>
2021-02-03 23:49:30 +0300

    Add project files.

---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    //It is a class which implement IEntity 
+   public interface IEntity
+    {
+    }
+}
