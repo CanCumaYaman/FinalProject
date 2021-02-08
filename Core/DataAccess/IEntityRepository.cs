adcd8b817d1a0fbd9a8be874fb0b49714e746ccf
CanCumaYaman <65663050+CanCumaYaman@users.noreply.github.com>
2021-02-08 17:06:07 +0300

    06.02.2021

--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -1,15 +1,17 @@
-﻿using Entities.Abstract;
+﻿
+using Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Text;
 
-namespace DataAccess.Abstract
+namespace Core.DataAccess
 {
     //generic constraint
     //class :class olabilir değil referans tip olabilir demek
     //IEntity : IEntity olabilir yada Ientity implemete eden bir nesne olabilir
     //new(): newlenebilir olmalı
+    //Core katmanının amacı diğer katmanlara bağımsız olmak o yüzden proje referansı almaz
    public interface IEntityRepository<T> where T:class,IEntity,new()
     {
         List<T> GetAll(Expression<Func<T,bool>> filter=null);
--- Core/Entities/IEntity.cs
+++ Core/Entities/IEntity.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Entities.Abstract
+namespace Core.Entities
 {
     //It is a class which implement IEntity 
    public interface IEntity
